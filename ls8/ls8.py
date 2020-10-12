cc664c958a4114a35c366e8599725263e69cc7f8
Christopher Shockley <cishockleysr@gmail.com>
2020-10-11 21:35:13 -0400

    working

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,11 +1,17 @@
-#!/usr/bin/env python3
-
 """Main."""
 
 import sys
 from cpu import *
 
-cpu = CPU()
+if len(sys.argv) != 2:
+    print("Usage: ls8.py 'valid file path'")
 
-cpu.load()
-cpu.run()
+else:
+    try:
+        cpu = CPU()
+
+        cpu.load(sys.argv[1])
+        cpu.run()
+
+    except FileNotFoundError:
+        print(f"Couldn't open {sys.argv[1]}")
